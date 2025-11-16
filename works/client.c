ff2f2e245d549383a33a7e20ba1b3e6ca29574f2
x446688 <xzp2byf924@privaterelay.appleid.com>
2025-11-16 13:00:58 +0300

    Polished client.c and added IP input

--- works/client.c
+++ works/client.c
@@ -7,12 +7,16 @@
 
 int main(int argc, char const* argv[])
 {
+    char const ip[INET_ADDRSTRLEN];
+    printf("Введите IP-адрес сервера: ");
+    scanf("%s", &ip);
+    printf("Стучимся до %s\n", ip);
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
-    char* hello = "Hello from client";
+    char* hello = "Лидский А. А. ККСО-21-24 2 курс [КЛИЕНТ]";
     char buffer[1024] = { 0 };
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
+        printf("\n Ошибка при создании сокета \n");
         return -1;
     }
 
@@ -21,27 +25,22 @@ int main(int argc, char const* argv[])
 
     // Convert IPv4 and IPv6 addresses from text to binary
     // form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-        <= 0) {
-        printf(
-            "\nInvalid address/ Address not supported \n");
+    int err;
+    if (err = inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+        printf("\nНекорректный адрес/Адрес не поддерживается: %s\n", ip);
         return -1;
     }
 
-    if ((status
-         = connect(client_fd, (struct sockaddr*)&serv_addr,
-                   sizeof(serv_addr)))
-        < 0) {
-        printf("\nConnection Failed \n");
+    if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+        printf("\nОшибка подключения \n");
         return -1;
     }
   
     // subtract 1 for the null
     // terminator at the end
     send(client_fd, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    valread = read(client_fd, buffer,
-                   1024 - 1); 
+    printf("Сообщение отправлено!\n");
+    valread = read(client_fd, buffer, 1024 - 1); 
     printf("%s\n", buffer);
 
     // closing the connected socket
